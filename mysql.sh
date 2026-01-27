92b2477b6e4e6d6084b2e38c30e144ebc4163af9
vineethapotladurthi <vineethapotladurthi71@gmail.com>
2026-01-27 19:23:47 +0530

    updating..

--- mysql.sh
+++ mysql.sh
@@ -17,16 +17,16 @@ mkdir -p $LOGS_FOLDER
 # fi
 Check_Root(){
     if [ $USERID -ne 0 ]; then
-     echo -e " $R you have to run the script under root previleges.." &>>$LOGFILE
+     echo -e " ${R} you have to run the script under root previleges.." &>>$LOGFILE
      exit 1
     fi
 }
 validate(){
     if [ $1 -ne 0 ]; then
-     echo -e "${R} $2 failedd.......!" | tee -a $LOGFILE
+     echo -e "$2 ....${R}failedd.......!" | tee -a $LOGFILE
      exit 1
     else
-        echo -e "${G} $2 here you go successfully...." | tee -a &>>$LOGFILE
+        echo -e "$2...${G}.. here you go successfully...." | tee -a &>>$LOGFILE
     fi
 }
 Check_Root
