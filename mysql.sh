1d6894560bf4967a598eeba686e23e2abc919580
vineethapotladurthi <vineethapotladurthi71@gmail.com>
2026-01-28 18:49:38 +0530

    updating..

--- mysql.sh
+++ mysql.sh
@@ -6,17 +6,13 @@ mkdir -p $LOGS_FOLDER
 SCRIPT_NAME=$(basename $0 | cut -d "." -f1)
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
+touch "$LOG_FILE"
 
 USERID=$(id -u)
 Red='\033[0;31m'
 Green='\033[0;32m'
 Normal='\033[0m'
 
-
-# if [ $USERID -ne 0 ]; then
-#     echo "you have to run the script under root previleges.."
-#     exit 1
-# fi
 Check_Root(){
     if [ $USERID -ne 0 ]
     then
@@ -53,39 +49,3 @@ then
 else
     echo -e "${Green}...already settuped.."
 fi
-
-# for package in $@
-# do 
-#         echo "the  pakacge are $package" &>>LOGFILE
-#         dnf list installed $package
-#     if [ $? -ne 0 ]; then
-#         echo -e " $R $package was not installed on your device let us install $package"&>>LOGFILE
-#         dnf install $package -y
-#     # if [ $? -ne 0 ]; then
-#     #     echo "git installation was not success"
-#     #     exit 1
-#     # else
-#     #     echo " git installed succcessfully"
-#     # fi
-#         validate $? "$package installation"
-#     else
-#         echo  -e "$N $package was already iinstalled" | tee -a &LOGFILE
-#     fi
-# done
-
-# dnf list installed mysql
-
-#  if [ $? -ne 0 ]
-#   then
-#     echo -e "$R $package was not installed on your device let us install $package"&>>LOGFILE
-#     dnf install $package -y
-#     if [ $? -ne 0 ]
-#      then
-#         echo -e "$R $package installation was not success"&>>LOGFILE
-#         exit 1
-#     else
-#         echo -e "$G $package installed succcessfully"&>>LOGFILE
-#     fi
-# else
-#     echo -e "$N $package was already iinstalled"&>>LOGFILE
-# fi
